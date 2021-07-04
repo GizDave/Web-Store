afccda35e40e3def318a77a21db0ec9389de61fd
GizDave <david.shaw@nyu.edu>
2021-07-03 23:28:12 -0400

    Set nativeQuery = true

--- webstore-backend/src/main/java/com/sg/mthree/webstore/model/dao/ProductRepository.java
+++ webstore-backend/src/main/java/com/sg/mthree/webstore/model/dao/ProductRepository.java
@@ -2,13 +2,16 @@ package com.sg.mthree.webstore.model.dao;
 
 public interface ProductRepository extends JpaRepository<Product,Integer>{
     // get popular products by number of units ordered (not the best metric)
-    @Query("SELECT p from Product p WHERE p.productid IN (SELECT productid FROM Customer_Order_Product_Bridge GROUP BY productid SORT BY SUM(quantity) DESC LIMIT ?1%)")
+    @Query("SELECT p from Product p WHERE p.productid IN (SELECT productid FROM Customer_Order_Product_Bridge GROUP BY productid SORT BY SUM(quantity) DESC LIMIT ?1%)",
+            nativeQuery = true)
     List<Product> findByPopularity(int num_product);
 
-    @Query("SELECT p from Product p WHERE p.name LIKE ?1% ORDER BY p.name ?4% OFFSET ?2%*?3% LIMIT ?3%")
+    @Query("SELECT p from Product p WHERE p.name LIKE ?1% ORDER BY p.name ?4% OFFSET ?2%*?3% LIMIT ?3%",
+            nativeQuery = true)
     List<Product> findByName(String name, int page_num, int num_product, String order);
 
     // pagination
-    @Query("SELECT p from Product p ORDER BY p.?1% ?%4% ORDER BY p.name ?4% OFFSET ?2%*?3% LIMIT ?3%")
+    @Query("SELECT p from Product p ORDER BY p.?1% ?%4% ORDER BY p.name ?4% OFFSET ?2%*?3% LIMIT ?3%",
+            nativeQuery = true)
     List<Product> nextBatch(String filter, int page_num, int num_product, String order);
 }
--- webstore-backend/src/main/java/com/sg/mthree/webstore/model/dao/UserRepository.java
+++ webstore-backend/src/main/java/com/sg/mthree/webstore/model/dao/UserRepository.java
@@ -1,7 +1,8 @@
 package com.sg.mthree.webstore.model.dao;
 
 public interface UserRepository extends JpaRepository<User,Integer>{
-    @Query("SELECT CASE WHEN u.password = ?2% THEN u.userid ELSE NULL END FROM User u WHERE u.username = ?1%")
+    @Query("SELECT CASE WHEN u.password = ?2% THEN u.userid ELSE NULL END FROM User u WHERE u.username = ?1%",
+            nativeQuery = true)
     Integer login(String username, String password);
 
     @Query("SELECT u.isadmin FROM User u WHERE u.userid = ?1%")
