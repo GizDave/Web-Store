4d624b466294baf480e5e3c709e07fa50c9d06c8
GizDave <david.shaw@nyu.edu>
2021-07-06 10:11:45 -0400

    Update MainPage.java

--- webstore-backend/src/main/java/com/sg/mthree/webstore/controller/MainPage.java
+++ webstore-backend/src/main/java/com/sg/mthree/webstore/controller/MainPage.java
@@ -1,12 +1,16 @@
 package main.java.com.sg.mthree.webstore.controller;
 
+import main.java.com.sg.mthree.webstore.model.dao.ImageRepository;
 import main.java.com.sg.mthree.webstore.model.dao.ProductRepository;
+import main.java.com.sg.mthree.webstore.model.dto.Image;
 import main.java.com.sg.mthree.webstore.model.dto.Product;
+import main.java.com.sg.mthree.webstore.model.dto.Thumbnail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,8 +19,29 @@ public class MainPage {
     @Autowired
     private ProductRepository productDB;
 
+    @Autowired
+    private ImageRepository imageDB;
+
     @GetMapping("/featuredProducts")
-    public List<Product> getFeaturedProducts(){
-        return productDB.findByPopularity(5);
+    public List<Thumbnail> getFeaturedProducts(){
+        List<Product> tempP = productDB.findByPopularity(5);
+        List<Thumbnail> buffer = new ArrayList<>();
+        List<Image> tempI = null;
+
+        for(Product p: tempP) {
+            Thumbnail tn = new Thumbnail();
+            tn.setProductid(p.getProductid());
+            tn.setName(p.getName());
+            tn.setDescription(p.getDescription());
+
+            tempI = imageDB.findByProductId(p.getProductid());
+            if(tempI.size() > 0){
+                tn.setImage_path(tempI.get(0).getImage_path()); // default to first image of the product
+            }
+
+            buffer.add(tn);
+        }
+
+        return buffer;
     }
 }
