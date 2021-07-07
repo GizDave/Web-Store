2161b61f84b1a58b6eb78d500058d3635d577805
Chuan Lin <chuan.lin.cl@gmail.com>
2021-07-07 12:16:36 -0400

    I tried to list out some credit card results.

---
+++ webstore-frontend/components/CreditCardCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="">
+        <p>{{ccnumber}}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: "CreditCardNumber",
+    props:
+    {"id": {
+        type: Number,
+        default: 0
+    },
+    "ccnumber": {
+        type: String,
+        default: ""}
+    }
+}
+</script>
+<style>
+</style>
