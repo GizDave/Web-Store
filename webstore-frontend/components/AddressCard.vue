205282be4724283dd9b5b942397c70629e98ec09
Chuan Lin <chuan.lin.cl@gmail.com>
2021-07-07 13:15:39 -0400

    added gets for addresses

--- webstore-frontend/components/AddressCard.vue
+++ webstore-frontend/components/AddressCard.vue
@@ -1,9 +1,41 @@
 <template>
-    <div>
+    <div class="">
+        <div>{{id}}</div>
+        <div>{{street}}</div>
+        <div>{{city}}</div>
+        <div>{{state}}</div>
+        <div>{{zipcode}}</div>
+        <div>{{country}}</div>
     </div>
 </template>
 <script>
 export default {
+    name: "CreditCardNumber",
+    props:
+    {"id": {
+        type: Number,
+        default: 0
+    },
+    "street": {
+        type: String,
+        default: ""
+    },
+    "city": {
+        type: String,
+        default: ""
+    },
+    "state": {
+        type: String,
+        default: ""
+    },
+    "zipcode": {
+        type: String,
+        default: ""
+    },
+    "country": {
+        type: String,
+        default: ""}
+    }
 }
 </script>
 <style>
